663caf7ac77ba8d9477aa68e8d6603a067bf38ae
AJITH S <128063551+Ajith0405@users.noreply.github.com>
2023-08-07 20:58:09 +0530

    second commit

--- firstProg.ts
+++ firstProg.ts
@@ -5,3 +5,11 @@ function greeting(user){
 let people = "Ajith";
 
 console.log(greeting(people));
+
+function good(user){
+    return "Welcome "+user;
+}
+
+let person = "Ajith";
+
+console.log(good(person));
