6e4cc4741093fb23e84de946dfc8b65f47800e01
AJITH S <128063551+Ajith0405@users.noreply.github.com>
2023-08-08 17:41:00 +0530

    first commit

---
+++ TypeScript_FULL/T_1_TS_Exemplified.ts
@@ -0,0 +1,13 @@
+// variable annotations 
+let mark:number = 97;
+let studName:string = "Arun";
+let grade:string = 'A';
+let pass:boolean = true;
+
+/*
+    //check Type error 
+        mark = "98"; //Type 'string' is not assignable to type 'number'.
+
+        studName = 45; //Type 'number' is not assignable to type 'string'.
+
+*/
