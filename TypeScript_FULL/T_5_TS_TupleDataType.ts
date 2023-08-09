add6ce353dc9d2c0fb3b1524c746b83edea14a4e
AJITH S <128063551+Ajith0405@users.noreply.github.com>
2023-08-09 14:15:34 +0530

    first commit

---
+++ TypeScript_FULL/T_5_TS_TupleDataType.ts
@@ -0,0 +1,6 @@
+/*
+    In TypeScript, a tuple is a data structure that allows you to store a fixed-size collection of elements of different types. Unlike arrays, where all elements are typically of the same type, a tuple lets you specify a specific type for each element at a specific index. Tuples are useful when you want to represent a structured set of values where the position of each value is significant.
+*/
+const marks:[string,number] =["English",89]; // only limited value
+    console.log(marks);
+    
