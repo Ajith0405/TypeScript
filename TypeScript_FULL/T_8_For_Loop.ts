75be112b0e4766dde5ac0a25c8ade1bd2615cc9c
AJITH S <128063551+Ajith0405@users.noreply.github.com>
2023-08-17 04:48:54 +0530

    first commit

---
+++ TypeScript_FULL/T_8_For_Loop.ts
@@ -0,0 +1,10 @@
+//for loop
+    for(let i:number =0; i<5; i++){
+        console.log(i);
+    }
+
+    let nums:number[] =[10,20,30,40,50];
+    for(let i:number=0;i<nums.length;i++){
+        console.log(nums[i]);
+    }
+
